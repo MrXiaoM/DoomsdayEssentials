a68f28383549cd5c1c40bf79b0b13b4921ac6b05
TheFatCat <2431208142@qq.com>
2021-12-28 22:58:54 +0800

    upload libraries

--- src/main/java/top/mrxiaom/doomsdayessentials/skills/ISkill.java
+++
@@ -1,9 +0,0 @@
-package top.mrxiaom.doomsdayessentials.skills;
-
-import org.bukkit.inventory.ItemStack;
-
-public interface ISkill {
-	boolean canItemStackRunSkill(ItemStack item);
-
-	void runSkill(Object data);
-}
